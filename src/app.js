de357aa23df1396ac097bf5efb5a495ae7ab6724
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-03-14 20:52:31 +0000

    implemetar logica de palos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const sticks = ["Corazones", "Picas", "Treboles", "Diamantes"];
+  const numbersValue = [
+    "As",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  console.log(sticks);
+  console.log(numbersValue);
+
+  const sticksIndex = sticks[Math.floor(Math.random() * sticks.length)];
+  const valuesIndex =
+    numbersValue[Math.floor(Math.random() * numbersValue.length)];
+  console.log(sticksIndex);
+  console.log(valuesIndex);
 };
